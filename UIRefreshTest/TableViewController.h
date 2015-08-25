0dcbe58eeff4eba29f58c48cb3d70f5c486719dc
Cloudox <cloudox@foxmail.com>
2015-08-25 17:14:04 +0800

    实现刷新

---
+++ UIRefreshTest/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  UIRefreshTest
+//
+//  Created by mac on 15/5/28.
+//  Copyright (c) 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+//定义显示的行数
+@property (nonatomic, assign)NSInteger items;
+
+@end
